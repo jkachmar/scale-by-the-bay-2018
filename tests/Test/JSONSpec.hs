30c7cd69d6100afb6f36b53427acd6c8a37540cc
Joe Kachmar <joseph.kachmar@symbiont.io>
2018-11-11 19:20:12 -0500

    Small change to tests

--- tests/Test/JSONSpec.hs
+++ tests/Test/JSONSpec.hs
@@ -1,16 +1,24 @@
 module Test.JSONSpec where
 
+-- External modules
 import           Data.Proxy
 import           Test.Aeson.GenericSpecs
 import           Test.Hspec
 
---
+-- Local modules
 import           Server                  (Admin, BasicUser, Moderator, User)
 
 spec :: Spec
 spec = do
   describe "Round Trip JSON tests" $ do
-    context "for the User data type"      $ roundtripADTSpecs (Proxy @User)
-    context "for the BasicUser data type" $ roundtripSpecs (Proxy @BasicUser)
-    context "for the Moderator data type" $ roundtripSpecs (Proxy @Moderator)
-    context "for the Admin data type"     $ roundtripSpecs (Proxy @Admin)
+    context "for the User data type" $
+      roundtripADTSpecs (Proxy @User)
+
+    context "for the BasicUser data type" $
+      roundtripADTSpecs (Proxy @BasicUser)
+
+    context "for the Moderator data type" $
+      roundtripADTSpecs (Proxy @Moderator)
+
+    context "for the Admin data type" $
+      roundtripADTSpecs (Proxy @Admin)
