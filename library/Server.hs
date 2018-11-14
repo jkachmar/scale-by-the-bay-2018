0d02ab2374f59ecd94cd93b5c381d1664bcd5212
Joe Kachmar <joseph.kachmar@symbiont.io>
2018-11-14 11:59:45 -0500

    Adds swagger generation

--- library/Server.hs
+++ library/Server.hs
@@ -1,13 +1,19 @@
 module Server where
 
 -- Miscellaneous external modules
+import           Control.Monad                        (when)
 import           Control.Monad.IO.Class
 import           Data.Aeson
 import           Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy                 as LBS
+import qualified Data.ByteString.Lazy.Char8           as LBS8
+import           Data.Proxy
 import           Data.Text                            (Text)
 import           Data.Time
 import           GHC.Generics
+import           Lens.Micro                           (mapped, (&), (.~), (?~))
+import           System.Directory                     (doesFileExist,
+                                                       removeFile)
 import           Text.Pretty.Simple
 
 -- Random data generation modules
@@ -20,11 +26,16 @@ import           Test.QuickCheck.Arbitrary.ADT        (ToADTArbitrary)
 import           Test.QuickCheck.Instances            ()
 
 -- Web server modules
+import           Data.Swagger                         hiding
+                                                       (SchemaOptions (..))
 import           Network.Wai.Handler.Warp             (run)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import           Servant.API
 import           Servant.API.Generic
+import           Servant.Server                       (Server, serve)
 import           Servant.Server.Generic
+import           Servant.Swagger                      (toSwagger)
+import           Servant.Swagger.UI.ReDoc
 
 --------------------------------------------------------------------------------
 -- Application data model
@@ -48,10 +59,10 @@ data Admin
   { aUsername     :: !Text
   , aPassword     :: !Text
   , aEmailAddress :: !Text
-  , aPromotedBy   :: !(Maybe Admin)
   , aActive       :: !Bool
   , aCreatedAt    :: !UTCTime
   , aUpdatedAt    :: !UTCTime
+  , aPromotedBy   :: !(Maybe Admin)
   } deriving (Eq, Generic, Show)
 
 -- | A moderator within the service, containing a list of sub-communities that
@@ -63,10 +74,10 @@ data Moderator
   , mPassword     :: !Text
   , mEmailAddress :: !Text
   , mGoverns      :: ![Text]
-  , mPromotedBy   :: !Admin
   , mActive       :: !Bool
   , mCreatedAt    :: !UTCTime
   , mUpdatedAt    :: !UTCTime
+  , mPromotedBy   :: !Admin
   } deriving (Eq, Generic, Show)
 
 -- | A "basic" user within the service.
@@ -113,10 +124,10 @@ instance Arbitrary Admin where
         <$> arbitrary
         <*> arbitrary
         <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
+        <*> arbitrary
         <*> pure Nothing
-        <*> arbitrary
-        <*> arbitrary
-        <*> arbitrary
 
 -- An `Arbitrary` instance for `BasicUser` that automatically derives the
 -- typeclass methods required to generate random `BasicUser`s using the
@@ -162,22 +173,22 @@ instance ToJSON Admin
 --------------------------------------------------------------------------------
 -- Modified Generic JSON serialiation and deserialization
 --------------------------------------------------------------------------------
--- -- | Modifications to Aeson's generic JSON encoders/decoders to make the
--- -- | resulting output JSON a little more idiomatic.
--- -- |
--- -- | Takes an input function that modifies the record field labels before
--- -- | converting them from "CamelCase" to "snake_case".
--- -- |
--- -- | Additionally it, removes the `'` character from any constructor names,
--- -- | converts all constructor names from "CamelCase" to "snake_case", and
--- -- | wraps the generated JSON in an object whose key is the constructor name.
--- idiomaticJsonOptions :: (String -> String) -> Options
--- idiomaticJsonOptions f = defaultOptions
---   { constructorTagModifier = camelTo2 '_' . filter (not . (== '\''))
---   , fieldLabelModifier = camelTo2 '_' . f
---   , sumEncoding = ObjectWithSingleField
---   , tagSingleConstructors = True
---   }
+-- | Modifications to Aeson's generic JSON encoders/decoders to make the
+-- | resulting output JSON a little more idiomatic.
+-- |
+-- | Takes an input function that modifies the record field labels before
+-- | converting them from "CamelCase" to "snake_case".
+-- |
+-- | Additionally it, removes the `'` character from any constructor names,
+-- | converts all constructor names from "CamelCase" to "snake_case", and
+-- | wraps the generated JSON in an object whose key is the constructor name.
+idiomaticJsonOptions :: (String -> String) -> Options
+idiomaticJsonOptions f = defaultOptions
+  { constructorTagModifier = camelTo2 '_' . filter (not . (== '\''))
+  , fieldLabelModifier = camelTo2 '_' . f
+  , sumEncoding = ObjectWithSingleField
+  , tagSingleConstructors = True
+  }
 
 -- -- JSON serialization and deserialization typeclass instances for `User`s,
 -- -- slightly modified to produce more idiomatic JSON output
@@ -245,7 +256,7 @@ data UserRoutes route
   -- Equivalent to a "/users/basic" route that accepts GET requests and returns
   -- JSON
   , getBasicUsers :: route
-      :- Summary "Get a list of all 'basic' users."
+      :- Summary "Get a list of all basic users."
       :> Description "Get a list of all users currently registered with this \
                      \service who are not moderators or administrators."
       :> "users" :> "basic" :> Get '[JSON] [BasicUser]
@@ -278,7 +289,7 @@ userRouteHandlers = UserRoutes
   , getAdmins     = liftIO $ generate arbitrary
   }
 
--- | The API specification for all routes in the application.
+-- | The API specification for all non-documentation routes in the application.
 -- |
 -- | Note that the `Routes` record can, itself, contain route specifications.
 -- | This allows users to factor out components of the API type into different
@@ -296,9 +307,112 @@ routeHandlers = Routes
   }
 
 --------------------------------------------------------------------------------
--- | The entry point for the application.
+-- Swagger schema generation
+--------------------------------------------------------------------------------
+
+exampleAdmin :: Admin
+exampleAdmin =
+  let timestamp = UTCTime (fromGregorian 2018 1 1) 0
+  in Admin
+    { aUsername = "cvogt"
+    , aPassword = "123456"
+    , aEmailAddress = "cvogt@example.com"
+    , aActive = True
+    , aCreatedAt = timestamp
+    , aUpdatedAt = timestamp
+    , aPromotedBy = Nothing
+    }
+
+exampleModerator :: Moderator
+exampleModerator =
+  let timestamp = UTCTime (fromGregorian 2018 2 1) 0
+  in Moderator
+    { mUsername = "jkachmar"
+    , mPassword = "123456"
+    , mEmailAddress = "jkachmar@example.com"
+    , mGoverns = ["Scale by the Bay 2018"]
+    , mActive = True
+    , mCreatedAt = timestamp
+    , mUpdatedAt = timestamp
+    , mPromotedBy = exampleAdmin
+    }
+
+exampleBasicUser :: BasicUser
+exampleBasicUser =
+  let timestamp = UTCTime (fromGregorian 2018 3 1) 0
+  in BasicUser
+    { buUsername = "jkachmar"
+    , buPassword = "123456"
+    , buEmailAddress = "jkachmar@example.com"
+    , buActive = True
+    , buCreatedAt = timestamp
+    , buUpdatedAt = timestamp
+    }
+
+instance ToSchema User where
+  declareNamedSchema user =
+    let schemaOptions = fromAesonOptions $ idiomaticJsonOptions id
+    in genericDeclareNamedSchema schemaOptions user
+      & mapped.schema.description ?~ "An administrator, moderator, or basic \
+                                     \ user within this service."
+      & mapped.schema.example ?~ toJSON [ Admin' exampleAdmin
+                                        , Moderator' exampleModerator
+                                        , BasicUser' exampleBasicUser
+                                        ]
+
+instance ToSchema Admin where
+  declareNamedSchema admin =
+    let schemaOptions = fromAesonOptions $ idiomaticJsonOptions (drop 1)
+    in genericDeclareNamedSchema schemaOptions admin
+      & mapped.schema.description ?~ "An administrator within this service."
+      & mapped.schema.example ?~ toJSON exampleAdmin
+
+instance ToSchema Moderator where
+  declareNamedSchema moderator =
+    let schemaOptions = fromAesonOptions $ idiomaticJsonOptions (drop 1)
+    in genericDeclareNamedSchema schemaOptions moderator
+      & mapped.schema.description ?~ "A moderator within this service."
+      & mapped.schema.example ?~ toJSON exampleModerator
+
+instance ToSchema BasicUser where
+  declareNamedSchema basicUser =
+    let schemaOptions = fromAesonOptions $ idiomaticJsonOptions (drop 2)
+    in genericDeclareNamedSchema schemaOptions basicUser
+      & mapped.schema.description ?~ "A basic user within this service."
+      & mapped.schema.example ?~ toJSON exampleBasicUser
+
+-- | Swagger definition for this service, modified to provide additional
+-- | about the service.
+serviceSwagger :: Swagger
+serviceSwagger = toSwagger (genericApi $ Proxy @Routes)
+  & info.title       .~ "Scale by the Bay"
+  & info.version     .~ "1.0.0"
+  & info.license     ?~ ("Apache-2.0" & url ?~
+                         URL "https://www.apache.org/licenses/LICENSE-2.0")
+  & info.description ?~ "Hello, Scale by the Bay! This is small demo that \
+                        \demonstrates how one might go about sketching out  \
+                        \sketching out a data model for some idea in Haskell \
+                        \and incrementally turning it into a real, functioning \
+                        \web service."
+
+type AllRoutes
+  =    ToServantApi Routes
+  :<|> SwaggerSchemaUI "docs" "resources/swagger.json"
+
+allRouteHandlers :: Server AllRoutes
+allRouteHandlers =
+       genericServer routeHandlers
+  :<|> redocSchemaUIServer serviceSwagger
+
+--------------------------------------------------------------------------------
+-- | The entry point for this service.
 runServer :: IO ()
-runServer = run 8080 . logStdoutDev $ genericServe routeHandlers
+runServer = do
+  let relativePath = "resources/swagger.json"
+  swaggerExists <- doesFileExist relativePath
+  when swaggerExists $ removeFile relativePath
+  LBS8.writeFile relativePath $ encodePretty serviceSwagger
+  run 8080 . logStdoutDev $ serve (Proxy @AllRoutes) allRouteHandlers
 
 --------------------------------------------------------------------------------
 -- Helper functions to demosntrat arbitrary generation of datatypes _and_ their
